ab74ce3f9099ac12be0358f86ba2479dc2ba9214
Fu Yi <yifu@thoughtworks.com>
2020-02-15 21:02:16 +0800

    refactor getFibonacciByDigits method by recursion.

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -3,15 +3,11 @@ package cc.xpbootcamp.warmup.fibonacci;
 public class Fibonacci {
 
   public static int getFibonacciByDigits(int digits) {
-    if (digits == 7) {
-      return 13;
+
+    if (digits == 1 || digits == 2) {
+      return 1;
     }
-    if (digits == 4) {
-      return 3;
-    }
-    if (digits == 3) {
-      return 2;
-    }
-    return 1;
+
+    return getFibonacciByDigits(digits - 1) + getFibonacciByDigits(digits - 2);
   }
 }
