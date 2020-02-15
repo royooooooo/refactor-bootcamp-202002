3a3cc2e9abe35bab0c234277c026920fe61d6d00
Fu Yi <yifu@thoughtworks.com>
2020-02-15 21:58:14 +0800

    remove class description.

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,10 +1,5 @@
 package cc.xpbootcamp.warmup.cashier;
 
-/**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
- * price and amount. It also calculates the sales tax @ 10% and prints as part of order. It computes
- * the total order amount (amount of individual lineItems + total sales tax) and prints it.
- */
 public class OrderReceipt {
   public static double SALE_ROTE = .10;
 
