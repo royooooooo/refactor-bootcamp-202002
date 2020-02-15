23456ca453c88085e56c10b2c3bc8d4106a0115e
Fu Yi <yifu@thoughtworks.com>
2020-02-15 21:10:52 +0800

    return 12586269025L given 50, and use long type to replace int.

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -2,10 +2,10 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class Fibonacci {
 
-  public static int getFibonacciByDigits(int digits) {
+  public static long getFibonacciByDigits(int digits) {
 
     if (digits == 1 || digits == 2) {
-      return 1;
+      return 1L;
     }
 
     return getFibonacciByDigits(digits - 1) + getFibonacciByDigits(digits - 2);
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -1,5 +1,6 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import static cc.xpbootcamp.warmup.fibonacci.Fibonacci.getFibonacciByDigits;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -8,26 +9,31 @@ public class FibonacciTest {
 
   @Test
   void should_return_1_when_calculate_given_1() {
-    assertEquals(Fibonacci.getFibonacciByDigits(1), 1);
+    assertEquals(getFibonacciByDigits(1), 1L);
   }
 
   @Test
   void should_return_1_when_calculate_given_2() {
-    assertEquals(Fibonacci.getFibonacciByDigits(2), 1);
+    assertEquals(getFibonacciByDigits(2), 1L);
   }
 
   @Test
   void should_return_2_when_calculate_given_3() {
-    assertEquals(Fibonacci.getFibonacciByDigits(3), 2);
+    assertEquals(getFibonacciByDigits(3), 2L);
   }
 
   @Test
   void should_return_3_when_calculate_given_4() {
-    assertEquals(Fibonacci.getFibonacciByDigits(4), 3);
+    assertEquals(getFibonacciByDigits(4), 3L);
   }
 
   @Test
   void should_return_13_when_calculate_given_7() {
-    assertEquals(Fibonacci.getFibonacciByDigits(7), 13);
+    assertEquals(getFibonacciByDigits(7), 13);
+  }
+
+  @Test
+  void should_return_12586269025L_when_calculate_given_50() {
+    assertEquals(getFibonacciByDigits(50), 12586269025L);
   }
 }
