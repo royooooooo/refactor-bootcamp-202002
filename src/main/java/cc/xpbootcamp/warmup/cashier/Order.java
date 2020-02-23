322c9450a44ce448e3013ab0509831dabec837c0
Fu Yi <yifu@thoughtworks.com>
2020-02-23 17:54:40 +0800

    Revert "remove customer information from order receipt."
    This reverts commit f0b4b94c

--- src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/Order.java
@@ -3,11 +3,20 @@ package cc.xpbootcamp.warmup.cashier;
 import java.util.List;
 
 public class Order {
-
+  String customerName;
+  String address;
   List<LineItem> lineItems;
 
   public Order(List<LineItem> lineItems) {
     this.lineItems = lineItems;
+  }
+
+  public String getCustomerName() {
+    return customerName;
+  }
+
+  public String getCustomerAddress() {
+    return address;
   }
 
   public double getTotalSalesTax() {
